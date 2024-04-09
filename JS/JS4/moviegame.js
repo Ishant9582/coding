0afd4bce58c78efee98e9cc80e0dc93152a0b1ae
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2024-04-09 20:04:23 +0530

    d

--- JS/JS4/moviegame.js
+++ JS/JS4/moviegame.js
@@ -11,16 +11,22 @@ let guess = prompt("enter your guessed movie") ;
 //     console.log("you failed")
 // 
 //or
+let count = 0 ;
 while(true){
                 
 
 if(guess == a){
     console.log("congrats you suceed");
+    alert("you successfully guessed the movie in")
+    break ;
 }
 else if(guess== "quit" ){
     console.log("you failed") ;
+    break ;
 }
 else{
+    count++ ;
     guess = prompt("try again") ;
 }
 }
+
