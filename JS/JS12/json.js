3e8f710124fbdb0ba994f67bb1c183d34814a94c
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2024-04-12 18:12:53 +0530

    e

---
+++ JS/JS12/json.js
@@ -0,0 +1,14 @@
+let json = '{"activity":"Start a collection","type":"recreational","participants":1,"price":0,"link":"","key":"1718657","accessibility":0.5}'
+console.log(json.activity)   // undefined as trying to access json file like this
+// converting json to java
+let java = JSON.parse(json)
+console.log(java)
+console.log(java.activity)   // work properly as now it is java
+let a = {
+    name : "ishant" ,
+    class : 5 ,
+};
+console.log(a)
+// coverting java to json
+let d = JSON.stringify(a)
+console.log(d)
