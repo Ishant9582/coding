79975b571fedae470a21c78c9fb56131d8ea3d35
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2024-04-24 01:58:39 +0530

    P

--- JS PROJECTS/1.js
+++ JS PROJECTS/1.js
@@ -17,7 +17,12 @@ const clock = ()=>{
     inputs[3].value = Math.floor((diff)%60) ;
     }
     else{
-        document.querySelector("h3").innerText = "endate and current r same OR END IS ALREADY GONE" ;
+        document.querySelector("h3").innerText = ("THAT DAY IS PASSED AND U R CURRENTLY LEADING THAT TIME BY") ;
+
+        inputs[0].value = Math.floor((diff/86400)) ;
+        inputs[1].value = Math.floor((diff/3600)%24) ;
+        inputs[2].value = Math.floor((diff/60)%60) ;
+        inputs[3].value = Math.floor((diff)%60) ;
     }
 }
 setInterval(clock , 1000) ;
