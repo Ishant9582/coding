115e8fb82224d16f30d8b6661b2971a5c19aed07
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2024-04-24 01:22:31 +0530

    F

--- JS PROJECTS/1.js
+++ JS PROJECTS/1.js
@@ -1,4 +1,4 @@
-const enddate = "25 April 2024 10:00 PM"
+const enddate = prompt(' "enter in this format : 25 April 2024 10:00 PM" ')
 document.querySelector("h3").innerText = enddate ;
 let inputs = document.querySelectorAll("input")
 
@@ -10,10 +10,15 @@ const clock = ()=>{
     //divide by 1000 to make in seconds
     //console.log(diff) // 
     //convert into dates
+    if(diff>0){
     inputs[0].value = Math.floor((diff/86400)) ;
     inputs[1].value = Math.floor((diff/3600)%24) ;
     inputs[2].value = Math.floor((diff/60)%60) ;
     inputs[3].value = Math.floor((diff)%60) ;
+    }
+    else{
+        document.querySelector("h3").innerText = "endate and current r same" ;
+    }
 }
 setInterval(clock , 1000) ;
 clock()
