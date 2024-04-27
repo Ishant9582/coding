61321fe5d92caae3d5e367bdc454f2d0d0505984
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2024-04-27 13:14:28 +0530

    o

--- JS PROJECTS/password verifier/1.js
+++ JS PROJECTS/password verifier/1.js
@@ -4,22 +4,24 @@ let pass = document.querySelector(".k")
 btn.addEventListener("click" , ()=>{
     let verifyuser = localStorage.getItem("user")
     let passverify = localStorage.getItem("pass")
-    console.log(verifyuser) ;
+    
     if((verifyuser == "null")){
         if(passverify == "null"){
             let d = user.value  ;
             let b  = pass.value ;
             localStorage.setItem("pass" , JSON.stringify(b))
             localStorage.setItem("user",JSON.stringify(d))
+            user.value = "" ;
+            pass.value = "" ;
+            console.log("hello")
         }
     }
     else{
-            console.log(verifyuser.length)
+           
             let g = JSON.stringify((user.value))
             console.log(g.length)
             let d =  JSON.stringify((pass.value))
             console.log(d.length)
-            console.log(passverify.length)
             if((verifyuser == g)){
                 if(passverify == d){
                     user.value = "" ;
