51b907481dfe2c4e7b160c73449e47cb23a0e1c7
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2024-05-04 14:19:11 +0530

    gg

--- backend/EXPRESSDIR/index.js
+++ backend/EXPRESSDIR/index.js
@@ -5,15 +5,14 @@ const app = express();
 // console.dir(app)
 let port =  3000 ;
 app.listen(port , ()=>{
-    console.log("app is listening on port")
+    console.log("app is listening on port") ;
 }) ;
 app.get("/",(req,res)=>{
-    res.send("hello you r at root path")
+    res.send("hello you r at home")
 })
 app.get("/apple",(req,res)=>{
     res.send("you r at apple path")
 })
-
 app.get("/orange",(req,res)=>{
     res.send("you r at orange path")
 })
@@ -27,6 +26,11 @@ app.post("/",(req,res)=>{
 // app.use((req,res)=>{
 //     console.log("request receeived")
 //     let code = "<h1><u>Hello</u></h1><ul><li>one</li><li>two</li></ul>"
+// or 
+// let code = {
+//     a : "name" ,
+//     b: "hello" ,
+// }
 //     res.send(code) ; // for sending response
 //     //res.send("jello") this will not work as only one response
 // })
